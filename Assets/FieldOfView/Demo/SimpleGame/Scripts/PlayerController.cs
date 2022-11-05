48a3f86e89d801a892376afc2873736501cb7ba1
wadim55 <korkin_wadim@mail.ru>
2022-11-05 18:57:23 +0600

    NavMeshBestMamont

---
+++ Assets/FieldOfView/Demo/SimpleGame/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+    
+    
+    public GameObject NavigationSurface;
+    private NavMeshAgent _navMeshAgent;
+
+
+    private void Start() {
+        this._navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update() {
+        
+        
+        // mouse click
+        if (Input.GetMouseButtonUp(0)) {
+            MovePlayerTo(Input.mousePosition);
+        }
+        // tap on touch screen
+        else if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended) {
+            MovePlayerTo(Input.GetTouch(0).position);
+        }
+    }
+
+    private void MovePlayerTo(Vector3 position) {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(position);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject == this.NavigationSurface) {
+                this._navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
